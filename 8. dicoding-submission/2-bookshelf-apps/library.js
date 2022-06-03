8e89aad379b4c6d043da394c82d8320c24deb310
agilmanf <agilmanf@gmail.com>
2022-06-03 12:25:32 +0700

    bookshelf apps - finished

--- 8. dicoding-submission/2-bookshelf-apps/library.js
+++ 8. dicoding-submission/2-bookshelf-apps/library.js
@@ -73,6 +73,7 @@ cards.forEach((card) => {
     }
 
     printDatas();
+    _books = books;
   });
 });
 
@@ -101,10 +102,181 @@ function dataTemplate(book, index) {
     <td>${book.year}</td>
     <td>${status}</td>
     <td class="col-action">
-      <button class="edit-button">Edit</button>
-      <button class="delete-button">Delete</button>
+      <button class="edit-button" onclick="toggleModal(${book.id})">Edit</button>
+      <button class="delete-button" onclick="deleteData(${book.id})">Delete</button>
     </td>
   </tr>`;
 }
 
 printDatas();
+
+// Edit Data //
+
+function addData() {
+  this.event.preventDefault();
+  books = getAllBooks();
+
+  const judulInput = document.querySelector("#judul");
+  const penulisInput = document.querySelector("#penulis");
+  const tahunInput = document.querySelector("#tahun");
+  const selesaiInput = document.querySelector("#selesai");
+
+  const newBook = {
+    id: Date.now(),
+    title: judulInput.value,
+    author: penulisInput.value,
+    year: tahunInput.value,
+    isComplete: selesaiInput.checked,
+  };
+
+  if (checkDuplicate(judulInput.value)) {
+    return alert("Buku ini sudah ada di Library-mu, masukan judul yang lain!");
+  }
+
+  books.push(newBook);
+  localStorage.setItem("mybooks", JSON.stringify(books));
+
+  alert("Buku berhasil ditambahkan");
+  updateUI();
+  toggleModal();
+}
+
+function editData(id) {
+  this.event.preventDefault();
+  books = getAllBooks();
+
+  const judulInput = document.querySelector("#judul");
+  const penulisInput = document.querySelector("#penulis");
+  const tahunInput = document.querySelector("#tahun");
+  const selesaiInput = document.querySelector("#selesai");
+
+  const newBooks = books.map((book) => {
+    if (book.id === id) {
+      return {
+        ...book,
+        title: judulInput.value,
+        author: penulisInput.value,
+        year: tahunInput.value,
+        isComplete: selesaiInput.checked,
+      };
+    }
+    return book;
+  });
+
+  books = newBooks;
+  localStorage.setItem("mybooks", JSON.stringify(books));
+
+  updateUI();
+  toggleModal();
+}
+
+function deleteData(id) {
+  books = getAllBooks();
+  const newBooks = books.filter((book) => book.id !== id);
+  const confirmed = confirm("Apakah kamu yang yakin mau hapus buku ini?");
+
+  if (confirmed) {
+    books = newBooks;
+    localStorage.setItem("mybooks", JSON.stringify(books));
+    updateUI();
+  }
+}
+
+function checkDuplicate(title) {
+  return books.find((book) => book.title == title);
+}
+
+function updateUI() {
+  calculateTotalBooks();
+
+  // check active card //
+  cards.forEach((card) => {
+    if (card.classList.contains("active")) {
+      switch (card.id) {
+        case "card-incomplete": {
+          books = getIncompleteBooks();
+          break;
+        }
+        case "card-completed": {
+          books = getCompletedBooks();
+          break;
+        }
+        case "card-total": {
+          books = getAllBooks();
+          break;
+        }
+      }
+    }
+  });
+
+  printDatas();
+}
+
+// Modal System //
+const modal = document.querySelector(".modal-container");
+
+function toggleModal(id) {
+  modal.innerHTML = modalTemplate(id);
+  modal.classList.toggle("active");
+}
+
+function modalTemplate(id) {
+  const book = books.find((book) => book.id === id);
+  const header = book ? "Edit Data Buku" : "Tambah Buku Baru";
+
+  const judul = book?.title || "";
+  const penulis = book?.author || "";
+  const tahun = book?.year || "";
+  const selesai = book?.isComplete ? "checked" : "";
+
+  const formSubmit = book ? `editData(${book.id})` : `addData()`;
+
+  return `
+  <div class="modal-backdrop">
+        <div class="modal">
+          <button class="close-modal-button" onclick="toggleModal()">&#x2716;</button>
+          <h5>${header}</h5>
+          <span>Isi data buku untuk dimasukan ke library mu!</span>
+          <form onsubmit="${formSubmit}">
+            <label for="judul">
+              <span>Judul Buku</span>
+              <input id="judul" type="text" value="${judul}" required autocomplete="off" />
+            </label>
+            <label for="penulis">
+              <span>Penulis</span>
+              <input id="penulis" type="text" value="${penulis}" required autocomplete="off" />
+            </label>
+            <label for="tahun">
+              <span>Tahun</span>
+              <input id="tahun" type="number" value="${tahun}" required autocomplete="off" />
+            </label>
+            <label for="selesai" class="checkbox">
+              <span>Selesai Dibaca?</span>
+              <input id="selesai" type="checkbox" ${selesai} />
+            </label>
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+      </div>`;
+}
+////////////////////
+
+// Search //
+const searchForm = document.querySelector(".search-form");
+const searchInput = document.querySelector(".search-input");
+let _books = books;
+
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const keyword = searchInput.value;
+
+  const regex = new RegExp(`^.*${keyword}.*$`, "ig");
+  const filteredBooks = _books.filter((book) => {
+    if (book.title.match(regex) !== null) {
+      return book;
+    }
+  });
+
+  books = filteredBooks;
+  printDatas();
+});
